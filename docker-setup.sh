6c7284796a5ca71d76d946b9052dbf9fa01232e1
Han Fang <hanfang@amazon.com>
2024-11-22 16:16:38 -0800

    update docker setup script

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker stop $(docker ps -q)
+
 docker pull mcr.microsoft.com/playwright:v1.49.0-noble
 
 # docker run -it --privileged --rm -p 5173:5173 --ipc=host --tmpfs /tmp --tmpfs /var/log --tmpfs /var/run mcr.microsoft.com/playwright:v1.49.0-noble /bin/bash
