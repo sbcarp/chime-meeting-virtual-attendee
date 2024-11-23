2516ffae739014e349b5237b20cec1c45b031b7f
Han Fang <hanfang@amazon.com>
2024-11-22 16:14:02 -0800

    run docker in background

--- docker-setup.sh
+++ docker-setup.sh
@@ -4,6 +4,6 @@ docker pull mcr.microsoft.com/playwright:v1.49.0-noble
 
 # docker run -it --privileged --rm -p 5173:5173 --ipc=host --tmpfs /tmp --tmpfs /var/log --tmpfs /var/run mcr.microsoft.com/playwright:v1.49.0-noble /bin/bash
 
-docker run -it --privileged --rm -p 5173:5173 --ipc=host --tmpfs /tmp --tmpfs /var/log --tmpfs /var/run mcr.microsoft.com/playwright:v1.49.0-noble /bin/bash -c "
+docker run -itd --privileged --rm -p 5173:5173 --ipc=host --tmpfs /tmp --tmpfs /var/log --tmpfs /var/run mcr.microsoft.com/playwright:v1.49.0-noble /bin/bash -c "
     bash <(curl -fsSL https://raw.githubusercontent.com/sbcarp/chime-meeting-virtual-attendee/refs/heads/main/start.sh)
 "
